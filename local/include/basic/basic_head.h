a644a652ba3888ceb3fdc296a2ee879568d58a65
rjf12138 <3161173206@qq.com>
2021-10-17 16:23:33 +0800

    2021年 10月 17日 星期日 16:23:33 CST

--- local/include/basic/basic_head.h
+++ local/include/basic/basic_head.h
@@ -20,6 +20,34 @@
 #include <csignal>
 #include <set>
 
-using namespace std;
+//using namespace std;
+
+#if defined(_WIN32) || defined(_WIN64) || defined(__WIN32__) 
+    #define __RJF_WINDOWS__
+#elif defined(__gnu_linux__) || defined(__linux__)
+    #define __RJF_LINUX__
+#endif
+
+#if defined(__RJF_LINUX__)
+////////////// linux system header file ////////////////
+#include <pthread.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <setjmp.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/sysinfo.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#elif defined(__RJF_WINDOWS__)
+#include <window.h>
+#endif
 
 #endif
