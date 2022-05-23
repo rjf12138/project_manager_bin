d9019e440d1ca4a6c583d2eabd107efa89dff22f
rjf12138 <3161173206@qq.com>
2022-05-23 21:31:38 +0800

    2022年 05月 23日 星期一 21:31:38 CST

---
+++ local/include/investment/stock_define.h
@@ -0,0 +1,10 @@
+#ifndef __STOCK_DEFINE_H__
+#define __STOCK_DEFINE_H__
+
+#define STOCK_CODE_LIST_FILE        "./config/stocks/stocks.json"      // 股票列表文件
+#define STOCK_CODE_DATA_DIR         "./config/stocks/stock_infos/"                 // 股票数据存放目录
+#define STOCK_REQUEST_POS_FILE      "./config/stocks/pos.txt"               // 不能一直请求所以记录股票请求为止
+#define STOCK_TEST_OUTPUT_FILE      "./config/test.txt"                     // 股票测试数据输出目录
+#define STOCK_CHOOSE_CODE           "./config/result.txt"                   // 选中股票输出目录
+
+#endif
